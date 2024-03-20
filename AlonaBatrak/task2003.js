8040ba9e7d4e266e666dd5a5e1b1ecdbe9735644
Alona <batrakalena86@gmail.com>
2024-03-20 12:01:59 +0100

    add task2003 file

---
+++ AlonaBatrak/task2003.js
@@ -0,0 +1,6 @@
+db.articles.insertOne({
+   title: "My first article",
+   tags: ["happines", "spring", "joy"],
+   content: "Some interesting text",
+   author_id: ObjectId("65e8471eb0cf085e5c4f5443")
+})
