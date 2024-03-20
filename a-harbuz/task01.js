0589932ad8487c955769405ad93dd4e35bd0b361
pamail08@gmail.com <pamail08@gmail.com>
2024-03-20 12:16:54 +0100

    change my articel

--- a-harbuz/task01.js
+++ a-harbuz/task01.js
@@ -1,9 +1,9 @@
 //https://western-appeal-39b.notion.site/GenTech-14-Mar-20-2024-003ba3cee4734b6d91d65cbdb179f053
 
-db.articles.insertOne ([
-    {
-        title: 'Super Article',
-        $addToSet: {tags: {$each:['IntelliJ','run','play']} },
-        content: 'Чтобы запустить приложение IntelliJ IDEA, просто нажмите зеленую кнопку play на панели инструментов',
-        author_id: 1}
-])
+db.articles.insertOne ({
+    title: 'Super Article',
+    $addToSet: {tags: {$each:['IntelliJ','run','play']} },
+    content: 'Чтобы запустить приложение IntelliJ IDEA, просто нажмите зеленую кнопку play на панели инструментов',
+    author_id: 1
+})
+
