4e53845233532b2a03dc504dc5eefb7a12deb988
Viktor Bulatov <lightoton@gmail.com>
2024-03-20 12:12:42 +0100

    update task1

--- Viktor Bulatov/task1.js
+++ Viktor Bulatov/task1.js
@@ -1,3 +1,3 @@
 db.articles.insertOne(
-    {title: "Stars", tags: ["space", "stars", "science"], content: "fejwionoabnuovbnuorbnao", author_id: "1"}
+    {title: "Stars", tags: ["space", "stars", "science"], content: "fejwionoabnuovbnuorbnao", author_id: ObjectId("65e8475ddee19d3874f5e0e7")}
 )
