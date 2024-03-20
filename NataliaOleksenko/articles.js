4f0dda892cf07fbc6bfaf82a7a78f65d5009b4f2
natol2023 <moneta.ya@gmail.com>
2024-03-20 12:11:16 +0100

    changes for articles

--- NataliaOleksenko/articles.js
+++ NataliaOleksenko/articles.js
@@ -1,3 +1,3 @@
 db.articles.insertOne(
-    { title: 'My article', tags: 'test', content: 'random text', author_id: "1234") }
+    { title: 'My article', tags: 'test', content: 'random text', author_id: ObjectId("65e84746f973af3570bd8f77") }
 );
